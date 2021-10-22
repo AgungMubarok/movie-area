b9a8fae4d04db3b99e7a8547d9df994b30c146f7
Agung Mubarok <agungmubarok555@gmail.com>
2021-10-23 00:56:25 +0700

    update

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,13 @@
-import { createStore,applyMiddleware } from 'redux';
-// import rootReducer from './RootReducer';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+
 import rootReducer from '../reducers';
-const thunkMiddleware  = require('redux-thunk').default;
 
-
-const store = createStore(rootReducer,applyMiddleware(thunkMiddleware));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, thunk))
+);
 
 export default store;
